10946edb1f856fc090c3226d04ff73b2f82d6235
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-23 16:14:43 -0400

    add input field to set frequency and store in session

--- Morigato_Synth.js
+++ Morigato_Synth.js
@@ -1,7 +1,34 @@
 if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('frequency', 0);
+
+  Template.getFrequency.helpers({
+    frequency: function () {
+      return Session.get('frequency');
+    }
+  });
+
+  Template.setFrequency.events({
+    'keyup': function() {
+      console.log($('input')[0].value);
+      Session.set('frequency', $('input')[0].value);
+    }
+  })
 
   setTimeout(function() {
-    var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
+    var synth = T("SynthDef").play();
+
+    synth.def = function(opts) {
+      var VCO = T("saw", {freq:opts.freq});
+
+      var cutoff = T("env", {table:[8000, [opts.freq, 500]]}).bang();
+      var VCF    = T("lpf", {cutoff:cutoff, Q:5}, VCO);
+
+      var EG  = T("adsr", {a:150, d:500, s:0.45, r:1500, lv:0.6});
+      var VCA = EG.append(VCF).bang();
+
+      return VCA;
+    };
 
     var keydict = T("ndict.key");
     var midicps = T("midicps");
@@ -17,8 +44,7 @@ if (Meteor.isClient) {
         synth.noteOff(midi, 100);
       }
     }).start();
-  }, 10);
-
+  }, 500);
 }
 
 if (Meteor.isServer) {
