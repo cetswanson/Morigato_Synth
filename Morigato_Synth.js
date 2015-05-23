93a9982bf738dd751e454c8fdbe90710ca5378ab
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-23 15:39:16 -0400

    get midi to work on page load. Remove tutorial meteor items

--- Morigato_Synth.js
+++ Morigato_Synth.js
@@ -1,40 +1,24 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  setTimeout(function() {
+    var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    var keydict = T("ndict.key");
+    var midicps = T("midicps");
+    T("keyboard").on("keydown", function(e) {
+      var midi = keydict.at(e.keyCode);
+      if (midi) {
+        var freq = midicps.at(midi);
+        synth.noteOnWithFreq(freq, 100);
+      }
+    }).on("keyup", function(e) {
+      var midi = keydict.at(e.keyCode);
+      if (midi) {
+        synth.noteOff(midi, 100);
+      }
+    }).start();
+  }, 10);
 
-  Template.play_button.events({
-    'click button': function() {
-      var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
-
-      var keydict = T("ndict.key");
-      var midicps = T("midicps");
-      T("keyboard").on("keydown", function(e) {
-        var midi = keydict.at(e.keyCode);
-        if (midi) {
-          var freq = midicps.at(midi);
-          synth.noteOnWithFreq(freq, 100);
-        }
-      }).on("keyup", function(e) {
-        var midi = keydict.at(e.keyCode);
-        if (midi) {
-          synth.noteOff(midi, 100);
-        }
-      }).start();
-    }
-  });
 }
 
 if (Meteor.isServer) {
