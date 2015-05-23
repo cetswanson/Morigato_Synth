142822c72ba32f550f5eb1e4be92b335fe6e364d
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-23 16:52:47 -0400

    add attack, delay, sustain, release

--- Morigato_Synth.js
+++ Morigato_Synth.js
@@ -32,6 +32,10 @@ if (Meteor.isClient) {
 
   Session.setDefault('frequency', 100);
   Session.setDefault('table', 8000);
+  Session.setDefault('attack', 150);
+  Session.setDefault('delay', 500);
+  Session.setDefault('sustain', 0.45);
+  Session.setDefault('release', 1500);
 
   Template.setFrequency.events({
     'keyup': function() {
@@ -45,6 +49,30 @@ if (Meteor.isClient) {
     }
   })
 
+  Template.setAttack.events({
+    'keyup': function() {
+      Session.set('attack', $('.attack')[0].value);
+    }
+  })
+
+  Template.setDelay.events({
+    'keyup': function() {
+      Session.set('delay', $('.delay')[0].value);
+    }
+  })
+
+  Template.setSustain.events({
+    'keyup': function() {
+      Session.set('sustain', $('.sustain')[0].value);
+    }
+  })
+
+  Template.setRelease.events({
+    'keyup': function() {
+      Session.set('release', $('.release')[0].value);
+    }
+  })
+
   setTimeout(function() {
     var synth = T("SynthDef").play();
 
@@ -55,7 +83,7 @@ if (Meteor.isClient) {
       var cutoff = T("env", {table:[parseInt(Session.get('table')), [parseInt(Session.get('frequency')), 500]]}).bang();
       var VCF    = T("lpf", {cutoff:cutoff, Q:5}, VCO);
 
-      var EG  = T("adsr", {a:150, d:500, s:0.45, r:1500, lv:0.6});
+      var EG  = T("adsr", {a:parseInt(Session.get('attack')), d:parseInt(Session.get('delay')), s:parseFloat(Session.get('sustain')), r:parseInt(Session.get('release')), lv:0.6});
       var VCA = EG.append(VCF).bang();
 
       return VCA;
