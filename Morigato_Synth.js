86a8ab6a42c2a5210eb1c5e39eebffc7520f74a4
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-23 16:26:19 -0400

    get frequency of midi to change with input

--- Morigato_Synth.js
+++ Morigato_Synth.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('frequency', 0);
+  Session.setDefault('frequency', 100);
 
   Template.getFrequency.helpers({
     frequency: function () {
@@ -19,9 +19,10 @@ if (Meteor.isClient) {
     var synth = T("SynthDef").play();
 
     synth.def = function(opts) {
-      var VCO = T("saw", {freq:opts.freq});
 
-      var cutoff = T("env", {table:[8000, [opts.freq, 500]]}).bang();
+      var VCO = T("saw", {freq: parseInt(Session.get('frequency'))});
+
+      var cutoff = T("env", {table:[8000, [parseInt(Session.get('frequency')), 500]]}).bang();
       var VCF    = T("lpf", {cutoff:cutoff, Q:5}, VCO);
 
       var EG  = T("adsr", {a:150, d:500, s:0.45, r:1500, lv:0.6});
@@ -44,7 +45,38 @@ if (Meteor.isClient) {
         synth.noteOff(midi, 100);
       }
     }).start();
-  }, 500);
+  }, 500)
+
+  // setTimeout(function() {
+  //   var synth = T("SynthDef").play();
+
+  //   synth.def = function(opts) {
+  //     var VCO = T("saw", {freq: Session.get('frequency')});
+
+  //     var cutoff = T("env", {table:[8000, [Session.get('frequency'), 500]]}).bang();
+  //     var VCF    = T("lpf", {cutoff:cutoff, Q:5}, VCO);
+
+  //     var EG  = T("adsr", {a:150, d:500, s:0.45, r:1500, lv:0.6});
+  //     var VCA = EG.append(VCF).bang();
+
+  //     return VCA;
+  //   };
+
+  //   var keydict = T("ndict.key");
+  //   var midicps = T("midicps");
+  //   T("keyboard").on("keydown", function(e) {
+  //     var midi = keydict.at(e.keyCode);
+  //     if (midi) {
+  //       var freq = midicps.at(midi);
+  //       synth.noteOnWithFreq(freq, 100);
+  //     }
+  //   }).on("keyup", function(e) {
+  //     var midi = keydict.at(e.keyCode);
+  //     if (midi) {
+  //       synth.noteOff(midi, 100);
+  //     }
+  //   }).start();
+  // }, 500);
 }
 
 if (Meteor.isServer) {
