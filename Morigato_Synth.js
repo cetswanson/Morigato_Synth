7e999086e1529ab93c93cc2e215ca2ac73ea3f25
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-23 16:14:43 -0400

    add input field to set frequency and store in session

--- Morigato_Synth.js
+++ Morigato_Synth.js
@@ -29,6 +29,52 @@ if (Meteor.isClient) {
     sendNote(e.keyCode);
     playSound(e.keyCode);
   });
+
+  Session.setDefault('frequency', 0);
+
+  Template.getFrequency.helpers({
+    frequency: function () {
+      return Session.get('frequency');
+    }
+  });
+
+  Template.setFrequency.events({
+    'keyup': function() {
+      console.log($('input')[0].value);
+      Session.set('frequency', $('input')[0].value);
+    }
+  })
+
+  setTimeout(function() {
+    var synth = T("SynthDef").play();
+
+    synth.def = function(opts) {
+      var VCO = T("saw", {freq:opts.freq});
+
+      var cutoff = T("env", {table:[8000, [opts.freq, 500]]}).bang();
+      var VCF    = T("lpf", {cutoff:cutoff, Q:5}, VCO);
+
+      var EG  = T("adsr", {a:150, d:500, s:0.45, r:1500, lv:0.6});
+      var VCA = EG.append(VCF).bang();
+
+      return VCA;
+    };
+
+    var keydict = T("ndict.key");
+    var midicps = T("midicps");
+    T("keyboard").on("keydown", function(e) {
+      var midi = keydict.at(e.keyCode);
+      if (midi) {
+        var freq = midicps.at(midi);
+        synth.noteOnWithFreq(freq, 100);
+      }
+    }).on("keyup", function(e) {
+      var midi = keydict.at(e.keyCode);
+      if (midi) {
+        synth.noteOff(midi, 100);
+      }
+    }).start();
+  }, 500);
 }
 
 if (Meteor.isServer) {
