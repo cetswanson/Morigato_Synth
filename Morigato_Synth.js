3aa6cf23c4eb44ce263af3f26052a5c9a2d53469
Vinh Bui <bicyclethief@gmail.com>
2015-05-24 02:05:49 -0400

    Send message to clients and have each client play a midi note

--- Morigato_Synth.js
+++ Morigato_Synth.js
@@ -1,4 +1,26 @@
+
+stream = new Meteor.Stream('c2c');
+var RADIX = 10;
+
 if (Meteor.isClient) {
+
+  sendChat = function(message) {
+    stream.emit('message', message);
+    $('#message-board').prepend('<li>me: ' + message + '</li>');
+  };
+
+  stream.on('message', function(message) {
+    $('#message-board').prepend('<li>user: ' + message + '</li>');
+    var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
+
+    var keydict = T("ndict.key");
+    var midicps = T("midicps");
+    var midi = keydict.at(parseInt(message, RADIX));
+    if (midi) {
+      var freq = midicps.at(midi);
+      synth.noteOnWithFreq(freq, 100);
+    }
+  });
 
   setTimeout(function() {
     var synth = T("OscGen", {wave:"saw", mul:0.25}).play();
@@ -6,22 +28,27 @@ if (Meteor.isClient) {
     var keydict = T("ndict.key");
     var midicps = T("midicps");
     T("keyboard").on("keydown", function(e) {
-      var midi = keydict.at(e.keyCode);
-      if (midi) {
-        var freq = midicps.at(midi);
-        synth.noteOnWithFreq(freq, 100);
-      }
+
+      sendChat(e.keyCode);
+
     }).on("keyup", function(e) {
-      var midi = keydict.at(e.keyCode);
-      if (midi) {
-        synth.noteOff(midi, 100);
-      }
+
     }).start();
   }, 10);
 
 }
 
 if (Meteor.isServer) {
+
+  stream.permissions.write(function(eventName) {
+    return true;
+  });
+
+  stream.permissions.read(function(eventName) {
+    return true;
+  });
+
+
   Meteor.startup(function () {
     // code to run on server at startup
   });
